5c292f068e27ae570401e72e9c7aba5853802ea5
TobiWan8 <tobiaslarsen93@gmail.com>
2023-04-21 12:09:57 +0200

    Add project files.

---
+++ LAXApp/View/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace LAXApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
