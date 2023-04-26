37dfc4563bf2ae94a5b02483245e37de238163a3
TobiWan8 <tobiaslarsen93@gmail.com>
2023-04-26 12:21:23 +0200

    Database and Connectionstring done.

---
+++ LAXApp/MSSQL/ConnectionString.cs
@@ -0,0 +1,12 @@
+﻿namespace LAXApp.MSSQL
+{
+    internal class ConnectionString
+    {
+        private string _connectionString = @"Server = 192.168.23.212,1433; Initial Catalog = LAXMovieDB; persist security info = true; User ID=sa; Password=Passw0rd";
+        internal string ConnectionToSql
+        {
+            get { return _connectionString; }
+            set { _connectionString = value; }
+        }
+    }
+}
