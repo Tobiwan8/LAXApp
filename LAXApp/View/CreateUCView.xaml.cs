4315e88e359d5137fa5ccac5cb13b2c6edb4cbda
TobiWan8 <tobiaslarsen93@gmail.com>
2023-04-26 15:00:06 +0200

    Create methods for updating movies, genres and ratings

--- LAXApp/View/CreateUCView.xaml.cs
+++ LAXApp/View/CreateUCView.xaml.cs
@@ -1,28 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using LAXApp.MSSQL;
 
 namespace LAXApp.View
 {
-    /// <summary>
-    /// Interaction logic for CreateUCView.xaml
-    /// </summary>
     public partial class CreateUCView : UserControl
     {
         public CreateUCView()
         {
             InitializeComponent();
+            //BindGenres.UpdateGenres();
         }
     }
 }
--- LAXApp/View/EditUCView.xaml.cs
+++ LAXApp/View/EditUCView.xaml.cs
@@ -1,28 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using LAXApp.MSSQL;
 
 namespace LAXApp.View
 {
-    /// <summary>
-    /// Interaction logic for EditUCView.xaml
-    /// </summary>
     public partial class EditUCView : UserControl
     {
         public EditUCView()
         {
             InitializeComponent();
+            //BindMovies.UpdateMovies();
+            //BindGenres.UpdateGenres();
         }
     }
 }
