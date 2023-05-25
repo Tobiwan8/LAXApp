ab8769a1497c3b5e4bcd7a22090633004e04a336
TobiWan8 <tobiaslarsen93@gmail.com>
2023-05-25 07:54:38 +0200

    LAXApp Done

--- LAXApp/ViewModel/OverviewViewModel.cs
+++ LAXApp/ViewModel/OverviewViewModel.cs
@@ -8,13 +8,6 @@ namespace LAXApp.ViewModel
 {
     internal partial class OverviewViewModel : ObservableObject
     {
-        //[ObservableProperty]
-        //[NotifyPropertyChangedFor(nameof(MoviesList))]
-        //private List<Movie> movieList = new(BindGenresToCombobox.MoviesList());
-        //public List<Movie> MoviesList => MovieList;
-
-        //public List<Movie> Movies = MoviesList;
-
         [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(DGItemList))]
         private List<DataGridItem> dataGridItems = new(BindDataToDG.DGItemList());
